4e4a6516d235924d217081be0df5d75c674230bb
Gollavilli Dhanush Kumar <112778628+Dhanush2468@users.noreply.github.com>
2023-11-12 22:21:37 +0530

    v2

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwind from 'tailwindcss';
@@ -7,11 +8,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // If you are deploying to https://<USERNAME>.github.io/, for example your repository is at https://github.com/<USERNAME>, set base to '/'.
-
-  // If you are deploying to https://<USERNAME>.github.io/<REPO_NAME>/, for example your repository is at https://github.com/<USERNAME>/<REPO_NAME>, then set base to '/<REPO_NAME>/'.
-
-  base: '/gitprofile/',
+  base: '/', // Change this line to '/'
   plugins: [
     react(),
     VitePWA({
